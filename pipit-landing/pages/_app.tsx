caf44fddac7bde15d513872f58c0f53d5592bcf9
siyeon <siyeonleeme@gmail.com>
2021-02-19 02:11:11 +0900

    Add styled component

--- pipit-landing/pages/_app.tsx
+++ pipit-landing/pages/_app.tsx
@@ -1,5 +1,16 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from '../styles/reset';
+import theme from '../styles/theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;

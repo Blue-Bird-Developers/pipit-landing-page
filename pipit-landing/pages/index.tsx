68a26b57ee6cbda48bad548a76e60fede0a60ff7
seohyun0120 <sarahseohyun@gmail.com>
2021-02-24 00:51:40 +0900

    Add Head tag

--- pipit-landing/pages/index.tsx
+++ pipit-landing/pages/index.tsx
@@ -1,5 +1,13 @@
 import Head from 'next/head';
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Head>
+        <title>Pipit Landing Page</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      Hello
+    </div>
+  );
 }

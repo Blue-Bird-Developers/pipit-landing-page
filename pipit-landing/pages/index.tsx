1b6f88c6333aea2137f7fd650278b2856334f8c9
siyeon <sylee@megazone.com>
2021-03-08 23:07:50 +0900

    Apply fullpage.js

--- pipit-landing/pages/index.tsx
+++ pipit-landing/pages/index.tsx
@@ -1,13 +1,19 @@
 import Head from 'next/head';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactFullpage from '@fullpage/react-fullpage';
+import MainPage from './MainPage';
 
 export default function Home() {
   return (
+    <>
     <div>
       <Head>
         <title>Pipit Landing Page</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      Hello
     </div>
+          <MainPage />
+    </>
   );
 }
